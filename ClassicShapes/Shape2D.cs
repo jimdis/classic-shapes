6cdea895ad39014bd0434da7a3a8d02924bdb66a
Jim Disenstam <jim.disenstam@gmail.com>
2019-05-06 09:56:03 +0200

    Adding class Shape2D

--- ClassicShapes/Shape2D.cs
+++ ClassicShapes/Shape2D.cs
@@ -1,8 +1,68 @@
 using System;
 namespace ClassicShapes
 {
-    public abstract class Shape2d
+    public abstract class Shape2D : Shape
     {
+        private double _length;
+        private double _width;
+
+        private protected Shape2D(ShapeType ShapeType, double length, double width)
+        {
+            //Todo
+        }
+
+        public abstract double Area;
+
+        public abstract double Perimeter;
+
+        public double Length
+        {
+            get => _length;
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Length must be greater than zero.");
+                }
+                _length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => _width;
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Width must be greater than zero.");
+                }
+                _width = value;
+            }
+        }
+
+        public string ToString()
+        {
+            return ToString("G");
+        }
+
+        public string ToString(string format = "G")
+        {
+            if (format == "G")
+            {
+                return "Formaterar med G";
+            }
+            if (format == "R")
+            {
+                return "Formaterar med R";
+            }
+            else
+            {
+                throw new FormatException("format argument not accepted.")
+            }
+        }
 
     }
 }
