464459cad9014257fb037b8393f7439d92a4c715
Jim Disenstam <jim.disenstam@gmail.com>
2019-05-06 08:34:05 +0200

    Initial commit

---
+++ ClassicShapes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassicShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ClassicShapes/README.md
@@ -0,0 +1,5 @@
+# Classic Shapes
+
+Examinationsuppgift 2 i kursen 1dv024.
+
+Jim Disenstam
