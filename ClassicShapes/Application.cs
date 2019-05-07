59232ddefca3a09c44cfc55f8348cfce97736aef
Jim Disenstam <jim.disenstam@gmail.com>
2019-05-07 11:37:59 +0200

    Implementing ToString in Shape2D

--- ClassicShapes/Application.cs
+++ ClassicShapes/Application.cs
@@ -14,47 +14,69 @@ namespace ClassicShapes
         {
             try
             {
-                // LÄGG IN INPUT FÖR ATT SLUMPA!
-                Console.WriteLine("Implementing Rectangle with length 2 & width 4");
-                Rectangle shape2D = new Rectangle(2, 4);
-                Console.WriteLine($"Length: {shape2D.Length}");
-                Console.WriteLine($"Width: {shape2D.Width}");
-                Console.WriteLine($"Area: {shape2D.Area}");
-                Console.WriteLine($"Perimeter: {shape2D.Perimeter}");
-                Console.WriteLine($"ToString(R): {shape2D.ToString("R")}");
-                Console.WriteLine($"ToString(): {shape2D.ToString()}");
-                Console.WriteLine($"ToString(empty): {shape2D.ToString("")}");
-                Console.WriteLine($"ToString(null): {shape2D.ToString(null)}");
-                Console.WriteLine($"is3D: {shape2D.Is3D}");
+                Console.WriteLine("Välj typ av figur (\"2D\"/\"3D\"");
+                string figure = Console.ReadLine();
+                Console.WriteLine("Hur många figurer ska slumpas fram? (antal)");
+                int numberOfFigures = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Implementing Sphere with Diameter 4");
-                Sphere shape3D = new Sphere(4);
-                Console.WriteLine($"Length: {shape3D.Length}");
-                Console.WriteLine($"Width: {shape3D.Width}");
-                Console.WriteLine($"Height: {shape3D.Height}");
-                Console.WriteLine($"Diameter: {shape3D.Diameter}");
-                Console.WriteLine($"MantelArea: {shape3D.MantelArea}");
-                Console.WriteLine($"TotalSurfaceArea: {shape3D.TotalSurfaceArea}");
-                Console.WriteLine($"Volume: {shape3D.Volume}");
-                Console.WriteLine($"ToString(R): {shape3D.ToString("R")}");
-                Console.WriteLine($"ToString(): {shape3D.ToString()}");
-                Console.WriteLine($"ToString(empty): {shape3D.ToString("")}");
-                Console.WriteLine($"ToString(null): {shape3D.ToString(null)}");
-                Console.WriteLine($"is3D: {shape3D.Is3D}");
-                Console.WriteLine("Changing Width to 6");
-                shape3D.Width = 6;
-                Console.WriteLine($"Length: {shape3D.Length}");
-                Console.WriteLine($"Width: {shape3D.Width}");
-                Console.WriteLine($"Height: {shape3D.Height}");
-                Console.WriteLine($"Diameter: {shape3D.Diameter}");
-                Console.WriteLine($"MantelArea: {shape3D.MantelArea}");
-                Console.WriteLine($"TotalSurfaceArea: {shape3D.TotalSurfaceArea}");
-                Console.WriteLine($"Volume: {shape3D.Volume}");
-                Console.WriteLine($"ToString(R): {shape3D.ToString("R")}");
-                Console.WriteLine($"ToString(): {shape3D.ToString()}");
-                Console.WriteLine($"ToString(empty): {shape3D.ToString("")}");
-                Console.WriteLine($"ToString(null): {shape3D.ToString(null)}");
-                Console.WriteLine($"is3D: {shape3D.Is3D}");
+                Array values = Enum.GetValues(typeof(ShapeType));
+                Random random = new Random();
+
+                for (int i = 0; i < numberOfFigures; i++)
+                {
+                    ShapeType randomShape = (ShapeType)values.GetValue(random.Next(values.Length));
+                    // int shapeNumber = random.Next(ShapeType.GetNames(typeof(ShapeType)).Length);
+                    Console.WriteLine(randomShape);
+
+                    // var shape = Activator.CreateInstance(Type.GetType(randomShape.ToString()));
+
+                    // shape.
+                    // Console.WriteLine(shapeNumber);
+                    // Console.WriteLine((ShapeType)shapeNumber);
+
+                }
+
+                // LÄGG IN INPUT FÖR ATT SLUMPA!
+                // Console.WriteLine("Implementing Rectangle with length 2 & width 4");
+                // Rectangle shape2D = new Rectangle(2, 4);
+                // Console.WriteLine($"Length: {shape2D.Length}");
+                // Console.WriteLine($"Width: {shape2D.Width}");
+                // Console.WriteLine($"Area: {shape2D.Area}");
+                // Console.WriteLine($"Perimeter: {shape2D.Perimeter}");
+                // Console.WriteLine($"ToString(R): {shape2D.ToString("R")}");
+                // Console.WriteLine($"ToString(): {shape2D.ToString()}");
+                // Console.WriteLine($"ToString(empty): {shape2D.ToString("")}");
+                // Console.WriteLine($"ToString(null): {shape2D.ToString(null)}");
+                // Console.WriteLine($"is3D: {shape2D.Is3D}");
+
+                // Console.WriteLine("Implementing Sphere with Diameter 4");
+                // Sphere shape3D = new Sphere(4);
+                // Console.WriteLine($"Length: {shape3D.Length}");
+                // Console.WriteLine($"Width: {shape3D.Width}");
+                // Console.WriteLine($"Height: {shape3D.Height}");
+                // Console.WriteLine($"Diameter: {shape3D.Diameter}");
+                // Console.WriteLine($"MantelArea: {shape3D.MantelArea}");
+                // Console.WriteLine($"TotalSurfaceArea: {shape3D.TotalSurfaceArea}");
+                // Console.WriteLine($"Volume: {shape3D.Volume}");
+                // Console.WriteLine($"ToString(R): {shape3D.ToString("R")}");
+                // Console.WriteLine($"ToString(): {shape3D.ToString()}");
+                // Console.WriteLine($"ToString(empty): {shape3D.ToString("")}");
+                // Console.WriteLine($"ToString(null): {shape3D.ToString(null)}");
+                // Console.WriteLine($"is3D: {shape3D.Is3D}");
+                // Console.WriteLine("Changing Width to 6");
+                // shape3D.Width = 6;
+                // Console.WriteLine($"Length: {shape3D.Length}");
+                // Console.WriteLine($"Width: {shape3D.Width}");
+                // Console.WriteLine($"Height: {shape3D.Height}");
+                // Console.WriteLine($"Diameter: {shape3D.Diameter}");
+                // Console.WriteLine($"MantelArea: {shape3D.MantelArea}");
+                // Console.WriteLine($"TotalSurfaceArea: {shape3D.TotalSurfaceArea}");
+                // Console.WriteLine($"Volume: {shape3D.Volume}");
+                // Console.WriteLine($"ToString(R): {shape3D.ToString("R")}");
+                // Console.WriteLine($"ToString(): {shape3D.ToString()}");
+                // Console.WriteLine($"ToString(empty): {shape3D.ToString("")}");
+                // Console.WriteLine($"ToString(null): {shape3D.ToString(null)}");
+                // Console.WriteLine($"is3D: {shape3D.Is3D}");
 
             }
             catch (Exception ex)
--- ClassicShapes/Shape2D.cs
+++ ClassicShapes/Shape2D.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace ClassicShapes
 {
     public abstract class Shape2D : Shape
@@ -54,11 +56,24 @@ namespace ClassicShapes
         {
             if (format == "G" || String.IsNullOrEmpty(format))
             {
-                return "Formaterar med G";
+                string[] keys = { "Längd", "Bredd", "Omkrets", "Area" };
+                double[] values = { Length, Width, Perimeter, Area };
+                int maxLength = (int)values.Max().ToString().Length;
+                int padding = maxLength < 10 ? 10 : maxLength;
+                string align = "{0,-" + padding + "} {1," + padding + ":f1}\n";
+
+                String output = $"\n{ShapeType}\n---------------\n";
+
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    output += String.Format(align, $"{keys[i]}:", values[i]);
+                }
+
+                return output;
             }
             if (format == "R")
             {
-                return "Formaterar med R";
+                return $"{ShapeType}\t{Length:f1}\t{Width:f1}\t{Perimeter:f1}\t{Area:f1}";
             }
             else
             {
